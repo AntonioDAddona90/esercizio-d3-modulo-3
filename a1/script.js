cf365d76d592a82fb3d017c97f0512eb8368c95c
Antonio D'Addona <126093628+AntonioDAddona90@users.noreply.github.com>
2023-05-01 21:12:35 +0200

    Add files via upload

---
+++ a1/script.js
@@ -0,0 +1,22 @@
+document.getElementById("notizie").onclick = function() {
+    document.getElementById("notizie").remove();
+};
+function mouseover()
+{
+document.getElementById("vecchi").style.backgroundColor='blue';
+}
+function mouseout()
+{
+document.getElementById("vecchi").style.backgroundColor='white';
+}
+function message() {
+    alert("Questa pagina dice Marco");
+  document.getElementsById("Marco").alert
+}
+    
+
+      
+
+
+
+
